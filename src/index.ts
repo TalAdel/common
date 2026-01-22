76debe1225b646f29845cb129c4138fdc6b908b6
Tal Adel Faibish <taladelfaibish@MacBook-Pro-sl-Tal.local>
2026-01-22 15:43:38 +0200

    Updates

--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,27 @@ export type {
     CorrelationIdOptions,
     RequestWithCorrelation,
 } from './correlation/types';
+
+export type{
+    jwtPayload as JwtPayload,
+} from './services/jwt.service';
+
+export type{
+    CustomError,
+} from './errors/custom-error';
+
+export type{
+    default as Authenticate,
+} from './middlewares/auth-req';
+
+export type{
+    default as ErrorHandler,
+} from './middlewares/error-handler';
+
+export type{
+    correlationIdMiddleware as CorrelationIdMiddleware,
+} from './correlation/middleware';
+
+export type{
+    getCorrelationId as GetCorrelationId,
+} from './correlation/middleware';
