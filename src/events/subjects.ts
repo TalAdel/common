04bf79c59258a5c9d50ddd283b291805718887ce
Tal Adel Faibish <taladel@Tals-MacBook-Pro.local>
2026-02-08 19:04:22 +0200

    Updates

--- src/events/subjects.ts
+++ src/events/subjects.ts
@@ -2,16 +2,16 @@
 // Think of this as the "master list" of all possible events in the system.
 // If a topic isn't here, it doesn't exist.
 export enum Subjects {
-    UserCreated = 'user:created',
-    
-    ProductCreated = 'product:created',
-    ProductUpdated = 'product:updated',
-    ProductDeleted = 'product:deleted',
-    
-    OrderCreated = 'order:created',
-    OrderUpdated = 'order:updated',
-    OrderComplete = 'order:complete',
-    OrderExpired = 'order:expired',
-    
-    PaymentComplete = 'payment:complete',
-  }
+  UserCreated = 'user.created',  // Changed from 'user:created'
+  
+  ProductCreated = 'product.created',
+  ProductUpdated = 'product.updated',
+  ProductDeleted = 'product.deleted',
+  
+  OrderCreated = 'order.created',
+  OrderUpdated = 'order.updated',
+  OrderComplete = 'order.complete',
+  OrderExpired = 'order.expired',
+  
+  PaymentComplete = 'payment.complete',
+}
